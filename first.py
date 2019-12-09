25d019f95713df28689eb62413850a7202be73dc
Asketze <56710836+Asketze@users.noreply.github.com>
2019-12-09 15:25:25 +0200

    Пуляев Денис АИ-183 дз 1
    dz1

--- first.py
+++ first.py
@@ -1,30 +1,31 @@
-from random import randrange
-import time
-from datetime import datetime
+x = 1
+y = 2
+z = 5
+#Insertion Sort
+a = [741, 222, 456, 430, 124, 419, 101, 170, 930, 347, 179, 799, 461, 528, 870]
+j = 2
+for j in range(len(a)):
+    while j > 0 and a[j] > a[j - 1]:
+        a[j], a[j - 1] = a[j - 1], a[j]
+        j -= 1
+print(a)
 
-#buble
-my_list = [ randrange(0, 15) for i in range(10) ]
+if z <= 1 :
+    print("Hey")
+else:
+    print("Ho")
+#Sort by selection
+a = [792, 803, 982, 904, 436, 679, 583, 942, 85, 857, 812, 27, 95, 613, 674]
+for i in range(len(a) - 1):
+    for j in range(i + 1, len(a)):
+        if a[j] > a[i]:
+            a[i], a[j] = a[j], a[i]
+print(a)  
 
-max_list = len( my_list )
-
-i = 0
-while i < max_list:
-
-    j = 0
-    while j < max_list-i-1:
-
-        if my_list[ j ] > my_list[ j + 1 ]:
-
-            my_list[ j ], my_list[ j + 1] = my_list[ j + 1], my_list[ j ]
-        j+=1
-    i += 1
-
-print( my_list )
-
-
-
-from datetime import datetime
-start_time = datetime.now()
-# do your work here
-end_time = datetime.now()
-print('Duration: {}'.format(end_time - start_time))
+#Bubble sort
+a = [451, 196, 113, 593, 343, 314, 486, 79, 980, 648, 850, 900, 500, 46, 781]
+for i in range(len(a) - 1):
+    for j in range(len(a) - i - 1):
+        if a[j] < a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+print(a)
